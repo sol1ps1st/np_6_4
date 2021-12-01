8121c30c1b904c40f78d1b529d4e926516bc7b46
Alexandr Alberg <alalberg@yandex-team.ru>
2021-12-01 16:20:05 +0300

    правит порядок записей в маршрутах

--- news/urls.py
+++ news/urls.py
@@ -10,8 +10,8 @@ from news.views import (
 
 urlpatterns = [
     path('', NewsList.as_view(), name='post_list'),
-    path('<int:pk>', NewsPostDetail.as_view(), name='news_detail'),
-    path('create', NewsPostCreate.as_view(), name='news_create'),
-    path('update/<int:pk>', NewsPostUpdate.as_view(), name='news_update'),
-    path('delete/<int:pk>', NewsPostDelete.as_view(), name='news_delete'),
+    path('<int:pk>/', NewsPostDetail.as_view(), name='news_detail'),
+    path('create/', NewsPostCreate.as_view(), name='news_create'),
+    path('<int:pk>/update/', NewsPostUpdate.as_view(), name='news_update'),
+    path('<int:pk>/delete/', NewsPostDelete.as_view(), name='news_delete'),
 ]
