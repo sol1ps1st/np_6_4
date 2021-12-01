ecd0043c49adebe6fbe10292599236bcba73e5ad
Alexandr Alberg <alalberg@yandex-team.ru>
2021-12-01 15:50:52 +0300

    правит стили форм

--- news/forms.py
+++ news/forms.py
@@ -1,14 +1,29 @@
-from django.forms import (
-    ModelForm,
-)
+from django import forms
 
-from news.models import Post
+from news.models import Post, Category
 
 
-class PostForm(ModelForm):
+# class PostForm(ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['author'].empty_label = "Не выбран"
+#
+#     class Meta:
+#         model = Post
+#         fields = ['title', 'text', 'author', 'categories']
+
+
+class PostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['author'].empty_label = "Не выбран"
+        self.fields['title'].widget = forms.TextInput(attrs={'size': '60'})
+        self.fields['text'].widget = forms.Textarea(attrs={'cols': 80, 'rows': 10})
+        self.fields['categories'] = forms.ModelMultipleChoiceField(
+            queryset=Category.objects.all(),
+            widget=forms.CheckboxSelectMultiple,
+            label=Post._meta.get_field('categories').verbose_name,
+        )
 
     class Meta:
         model = Post
